24f9d7a26441a10142b25e1c779511e16785303c
GoDeKlein <tkachartyom51@gmail.com>
2020-11-04 01:17:30 +0300

    Оновлення коду на С++. Тепер можна із довжини в метрах дінатися цілу довжину в кілометрах

--- LR5/LR5.cpp
+++ LR5/LR5.cpp
@@ -6,10 +6,10 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     double L;
-    cout << "Введіть довжину в сантиметрах" << endl;
+    cout << "Введіть довжину в метрах" << endl;
     cin >> L;
-    L = static_cast<int> (L) / 100;
-    cout << "Довжина в метрах складає: " << L << endl;
+    L = static_cast<int> (L) / 1000;
+    cout << "Довжина в кілометрах складає: " << L << endl;
     system("pause");
 }
 
